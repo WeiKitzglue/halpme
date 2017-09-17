07e88094f505b07a5d88d48f89d51d60bf3426d4
Ken <ken@datatron.io>
2017-09-17 03:39:25 -0700

    merge

--- findme/views.py
+++ findme/views.py
@@ -3,14 +3,35 @@ from django.http import HttpResponse
 
 from datetime import datetime
 from models import Point
+import requests
 
 import logging
-# logger = logging.getLogger(__name__)
+import json
+
+FEATURELAYER_URL = 'https://services8.arcgis.com/j1t3CMZN0P8OmjOH/arcgis/rest/services/halp/FeatureServer/0'
 
 # Create your views here.
 def index(request):
-    print request.GET
-    return HttpResponse("HELLO")
+    points = Point.objects.all()
+    return render(request, 'findme/index.html', {'points': points})
+
+
+def post(lat, long, time):
+    feature = [
+        {
+            "geometry" : {"x" : lat, "y" : long},
+            "attributes" : {
+                "id" : 1,
+                "time": time
+            }
+        }
+    ]
+    logging.info(requests.post('{}/addFeatures'.format(FEATURELAYER_URL),
+                               data={
+                                   'f': 'json',
+                                   'features': json.dumps(feature)
+                               }))
+
 
 def incoming(request):
     logging.warning(request.GET)
@@ -25,3 +46,5 @@ def incoming(request):
         pass
 
     return HttpResponse("RECEIVING SMS")
+
+
